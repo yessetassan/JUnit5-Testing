96b3d20fb9f552abcc934d55dfedb0ac96e0a81b
yessetassan <139701904+yessetassan@users.noreply.github.com>
2024-02-14 12:31:30 +0600

    First Comment

---
+++ JUnit5Testing/src/main/java/com/project/JUnit5Testing/JUnit5TestingApplication.java
@@ -0,0 +1,13 @@
+package com.project.JUnit5Testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnit5TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JUnit5TestingApplication.class, args);
+	}
+
+}
---
+++ JUnit5Testing/src/test/java/com/project/JUnit5Testing/JUnit5TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.JUnit5Testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnit5TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
